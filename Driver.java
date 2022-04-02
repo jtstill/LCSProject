af20ab36eae16b2b0511ec27f8fccd8890d02ca9
jtstill <jtstill@loyola.edu>
2022-04-02 14:39:00 -0400

    executable main (currently loads the two test files)

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+
+    public static void main(String [] args) {
+
+        UserFile testfile1 = new UserFile("./LCSProject/testfile1.txt");
+        System.out.println("---------- FILE 1 ----------");
+        System.out.println(testfile1);
+
+        UserFile testfile2 = new UserFile("./LCSProject/testfile2.txt");
+        System.out.println("---------- FILE 2 ----------");
+        System.out.println(testfile2);
+    }
+}
