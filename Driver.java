a6d44d06308127da5fc0bf7071c7257f2d216da0
Jeremy Unanue <jrunanue@loyola.edu>
2022-04-05 12:47:08 -0400

    working on output

--- Driver.java
+++ Driver.java
@@ -1,16 +1,21 @@
 public class Driver {
-
-    public static void main(String [] args) {
-
-        UserFile testfile1 = new UserFile("./LCSProject/testfile1.txt");
-        System.out.println("---------- FILE 1 ----------");
-        System.out.println(testfile1);
-
-        UserFile testfile2 = new UserFile("./LCSProject/testfile2.txt");
-        System.out.println("---------- FILE 2 ----------");
-        System.out.println(testfile2);
-
-        LCSAlgorithm example = new LCSAlgorithm(testfile1, testfile2);
-        System.out.println(example.findLCS());
+    // had to modify directory so it works on my computer
+        public static void main(String [] args) {
+    
+    //        UserFile testfile1 = new UserFile("./LCSProject/testfile1.txt");
+            UserFile testfile1 = new UserFile("./testfile1.txt");
+    
+            System.out.println("---------- FILE 1 ----------");
+            System.out.println(testfile1);
+    
+    //        UserFile testfile2 = new UserFile("./LCSProject/testfile2.txt");
+            UserFile testfile2 = new UserFile("./testfile2.txt");
+    
+            System.out.println("---------- FILE 2 ----------");
+            System.out.println(testfile2);
+    
+            LCSAlgorithm example = new LCSAlgorithm(testfile1, testfile2);
+            //System.out.println(example.findLCS());
+            example.toString();
+        }
     }
-}
--- LCSAlgorithm.java
+++ LCSAlgorithm.java
@@ -1,13 +1,17 @@
-import java.lang.Math;
+//import java.lang.Math;
 
 public class LCSAlgorithm {
     
     protected UserFile f1;
     protected UserFile f2;
+    private int [][] t;
+    private String [] commonLines;
 
     public LCSAlgorithm(UserFile file1, UserFile file2){
         f1 = file1;
         f2 = file2;
+        t = findLCS();
+        commonLines = retrival(t);
     }
 
     // Gets the number of lines in the given file
@@ -15,8 +19,15 @@ public class LCSAlgorithm {
         return file.lines.size();
     }
 
+    public int numOfCommonLines()
+    {
+        //System.out.println("LCS length = " + t[t.length-1][t[0].length-1]);
+        return t[t.length-1][t[0].length-1];
+    }
+
     // the main algorithm
-    public String findLCS(){
+    // public String findLCS(){
+    private int [][] findLCS(){
         // build the table with f1 + 1 columns, f2 + 1 rows
         int [][] table = new int[totalLines(f1)+1][totalLines(f2)+1];
 
@@ -56,12 +67,60 @@ public class LCSAlgorithm {
         System.out.println("Printing table:");
         System.out.println(printTable(table));
 
+        // retrieve string array of LCS
+        //String[] temp = retrival(table);
+        
+        // temporary print the array
+        // for (String lines: temp) {
+        //     System.out.println(lines);
+        // }
+
         // recover information, starting at last cell
             // if left or up is equal to current cell, don't add and move (tiebreaker defaults to up)
             // if left or up are both less than current cell, go diagonal AND add to beginning of LCS
             // go until you hit 0
         
-        return "";
+        // return "";
+        return table;
+    }
+
+    // input: LCS solved table
+    // output String Array
+    // indexes from end of table
+    private String[] retrival(int[][] table)
+    {
+        int i = table.length -1;
+        int j = table[i].length -1;
+        String[] out = new String[table[i][j]];
+
+
+        // while cell your at does not = 0 (end)
+        while(table[i][j] > 0)
+        {
+            // define values
+            int left = table[i-1][j];
+            int top = table[i][j-1];
+            int diagonal = table[i-1][j-1];
+
+            // if top and left are the same -> check for matching substrings
+            if(top == left)
+            {
+                if (f1.lines.get(i-1).equals(f2.lines.get(j-1)))
+                {
+                    // diagnonal makes a good index for array placement
+                    out[diagonal] = f1.lines.get(i-1);
+                    i--;
+                    j--;
+                }
+
+                else // if equal and not a match -> move up one 
+                    i--;
+            }
+            
+            else if (top > left) j--;
+            else i--;
+        }
+        return out;
     }
 
     
@@ -91,10 +150,32 @@ public class LCSAlgorithm {
         
         return result;
     }
+    private String[] feedback(UserFile feedFile)
+    {
+        int fileIndex = 0;
+        int stringIndex = 0;
+        
+        return commonLines;
+    }
+
+    private String formattedPrint(String [] arr)
+    {
+        String out = "[";
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            out += arr[i] + ", ";
+        }
+        out += arr[arr.length-1] + "]";
+        return out;
+    }
 
     // toString method
     @Override
     public String toString(){
+        System.out.println("# of lines: " + numOfCommonLines());
+        System.out.println("Common lines are: \n" + formattedPrint(commonLines));
+        
+
         return "";
     }
 }
